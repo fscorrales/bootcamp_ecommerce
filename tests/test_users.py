732715c44524b1547ec0fcda35472f0fe70bc48d
Fernando Corrales <corrales_fernando@hotmail.com>
2024-09-07 09:38:08 -0300

    updata users tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,10 +4,34 @@ import pytest
 from bson import ObjectId
 from fastapi.testclient import TestClient
 
-from ..bootcamp_ecommerce.api.services import UsersServiceDependency
+from ..bootcamp_ecommerce.api.models import CreationUser
+from ..bootcamp_ecommerce.api.services import UsersServiceDependency, SecurityDependency
 from ..bootcamp_ecommerce.main import app
 
 client = TestClient(app)
+
+
+@pytest.fixture
+def dict_test_user() -> dict:
+    return {
+        "username": "Test",
+        "email": "test@test.com",
+        "role": "customer",
+        "password": "12345"
+    }
+
+
+@pytest.fixture
+def create_and_delete_user(dict_test_user):
+    user = CreationUser(**dict_test_user)
+    user_id = UsersServiceDependency().create_one(
+        user = user,
+        hash_password = user.password
+    )
+    yield user_id
+    UsersServiceDependency().delete_one_forever(
+            id = ObjectId(user_id)
+    )
 
 
 def test_create_user_without_body():
@@ -15,26 +39,21 @@ def test_create_user_without_body():
         "/api/Users/", 
         json={}
     )
-    assert response.status_code == 422
     print(response.json())
+    assert response.status_code == 422
 
 
-def test_create_user():
+def test_create_user(dict_test_user):
     response = client.post(
         "/api/Users/", 
-        json={
-            "username": "Test",
-            "email": "test@test.com",
-            "role": "customer",
-            "password": "12345"
-        }
+        json=dict_test_user
     )
+    print(response.json())
     assert response.status_code == 200
     if user_id:=response.json().get("user_id"):
         UsersServiceDependency().delete_one_forever(
             id = ObjectId(user_id)
         )
-    print(response.json())
 
 
 def test_get_all_active_users():
@@ -55,22 +74,67 @@ def test_get_all_active_users():
     }
 
     response = client.get("/api/Users/")
+    print(response.json())
     assert response.status_code == 200
     jsonschema.validate(
         instance=response.json(), schema=active_users_schema
     )
 
 
-def test_get_all_deleted_users():
-    pass
+def test_get_all_deleted_users_without_credentials():
+    response = client.get("/api/Users/deleted")
+    print(response.json())
+    assert response.status_code == 401
 
 
-def test_get_all_users():
-    pass
+@pytest.fixture
+async def override_security_dependency(dict_test_user):
+    class SecurityServiceOverride:
+        def __init__(self):
+            self.auth_user_id = ObjectId()
+            self.auth_user_name = dict_test_user.get("username")
+            self.auth_user_email = dict_test_user.get("email")
+            self.auth_user_role = 'admin'
+    return SecurityServiceOverride()
+
+@pytest.fixture
+def add_and_remove_security_dependency(override_security_dependency):
+    app.dependency_overrides[SecurityDependency] = override_security_dependency
+    yield
+    app.dependency_overrides = {}
 
 
-def test_get_one_user():
-    pass
+def test_get_all_users_deleted_users(add_and_remove_security_dependency):
+    response = client.get("/api/Users/deleted")
+    print(response.json())
+    assert response.status_code == 200
+
+
+def test_get_one_user(create_and_delete_user, dict_test_user):
+    user_id = create_and_delete_user
+    response = client.get(f"/api/Users/{user_id}")
+    print(response.json())
+    assert response.status_code == 200
+    created_user = dict_test_user
+    created_user["id"] = str(user_id)
+    created_user["deactivated_at"] = None
+    created_user["image"] = None
+    created_user.pop('password')
+    assert response.json() == created_user
+
+
+def test_get_one_user_invalid_id(create_and_delete_user):
+    user_id = "12345"
+    response = client.get(f"/api/Users/{user_id}")
+    print(response.json())
+    assert response.status_code == 422
+
+
+def test_get_one_user_with_nonexitent_id(create_and_delete_user):
+    user_id = ObjectId()
+    response = client.get(f"/api/Users/{user_id}")
+    print(response.json())
+    assert response.status_code == 404
 
 
 def test_update_user():
